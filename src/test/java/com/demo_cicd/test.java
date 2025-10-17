f06212a522049c1cc57a99c584f36a62d66a6717
yashgautam <yashsharma8668@gmail.com>
2025-10-17 16:58:17 +0530

    version2

---
+++ src/test/java/com/demo_cicd/test.java
@@ -0,0 +1,14 @@
+package com.demo_cicd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class test {
+	
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+
+}
